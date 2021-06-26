c01144a2d003775d4d4fac615e1789f7a6a53b49
Kotaro <swdevstudio@gmail.com>
2021-06-26 23:12:55 +0300

    add global scss

---
+++ src/components/teta.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="test">worl <span class="icon-account"></span></div>
+</template>
+
+<script>
+  export default {
+    name: "teta"
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+      }
+    }
+  },
+  pluginOptions: {
+
+    // 'style-resources-loader': {
+    //   preProcessor: 'scss',
+    //   patterns: [
+    //     './src/assets/*.scss'
+    //   ]
+    // }
+    // electronBuilder: {
+    //   nodeIntegration: true,
+    //   mainProcessWatch: ['src/scripts/server/*.ts']
+    // }
+  }
+}
